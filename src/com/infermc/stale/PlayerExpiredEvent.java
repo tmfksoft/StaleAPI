aec2258476ae4b89d5856f92e645f0c0dc85c7cf
Thomas Edwards <thomas.edwards@xeonr.com>
2015-05-31 23:47:35 +0100

    Fleshed out the main class a bit

---
+++ src/com/infermc/stale/PlayerExpiredEvent.java
@@ -0,0 +1,46 @@
+package com.infermc.stale;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.List;
+
+public final class PlayerExpiredEvent extends Event {
+
+    // Vars
+    private boolean cancelled = false;
+    private List<OfflinePlayer> players;
+
+    // Bukkit stuff
+    private static final HandlerList handlers = new HandlerList();
+
+    public PlayerExpiredEvent(List<OfflinePlayer> plist) {
+        players = plist;
+    }
+
+    /* Getters */
+
+    // Is the event cancelled?
+    public boolean isCancelled() {
+        return cancelled;
+    }
+    public List<OfflinePlayer> getPlayers(){
+        return players;
+    }
+
+    /* Setters */
+
+    // Cancel removing data for all listed players
+    public void setCancelled(boolean state) {
+        cancelled = state;
+    }
+
+    // Essential stuff.
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
--- src/com/infermc/stale/StaleAPI.java
+++ src/com/infermc/stale/StaleAPI.java
@@ -1,30 +1,59 @@
 package com.infermc.stale;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-/**
- * Created by Thomas on 31/05/2015.
- */
 public class StaleAPI extends JavaPlugin implements Listener {
+    private long threshold = 60; /* Default length of time since they last played */
+
     public void onEnable() {
         getLogger().info("StaleAPI Loaded. :D");
-        getServer().getPluginManager().registerEvents(new PlayerDataExpired(), this);
+        getServer().getPluginManager().registerEvents(this, this);
+
+        BukkitScheduler scheduler = getServer().getScheduler();
+        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                // Do something
+                getLogger().info("Running player checker.");
+                checkPlayers();
+            }
+        }, 0L, 120L);
     }
     // Skim through all players and check for expired ones.
     public void checkPlayers() {
-        List<UUID> players = new ArrayList<UUID>();
-        PlayerDataExpired event = new PlayerDataExpired(players);
-        getServer().getPluginManager().callEvent(event);
-        if (!event.isCanceled()) {
+        List<OfflinePlayer> players = new ArrayList<OfflinePlayer>();
+        for (OfflinePlayer p : getServer().getOfflinePlayers()) {
+            getLogger().info("Checking "+p.getUniqueId());
+            long secondsSince = epoch() - p.getLastPlayed();
+            if (secondsSince >= threshold) {
+                getLogger().info(p.getUniqueId()+" hasn't been on for "+secondsSince+" seconds, Higher than "+threshold);
+                players.add(p);
+            } else {
+                getLogger().info(p.getUniqueId()+" was last on "+secondsSince+" ago!");
+            }
+        }
 
+        if (players.size() > 0) {
+            PlayerExpiredEvent event = new PlayerExpiredEvent(players);
+            getServer().getPluginManager().callEvent(event);
+            if (!event.isCancelled()) {
+                // Remove the player data
+                getLogger().info("Removing data for all players.");
+            }
         }
     }
 
-    public void onExpire(PlayerDataExpired event) {
+    public void onExpire(PlayerExpiredEvent event) {
+        getLogger().info("Expire event called, Im a child plugin.");
+    }
+
+    private long epoch() {
+        return System.currentTimeMillis()/1000;
     }
 }
