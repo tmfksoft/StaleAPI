d5cf4a132f53af6c264ef02a249d23dadb4ab529
Thomas Edwards <thomas.edwards@xeonr.com>
2015-06-01 00:07:31 +0100

    Removed a lot of debug and commented the code

--- src/com/infermc/stale/StaleAPI.java
+++ src/com/infermc/stale/StaleAPI.java
@@ -1,56 +1,68 @@
 package com.infermc.stale;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StaleAPI extends JavaPlugin implements Listener {
     private long threshold = 60; /* Default length of time since they last played */
+    private long delay = 1200L;
 
     public void onEnable() {
         getLogger().info("StaleAPI Loaded. :D");
         getServer().getPluginManager().registerEvents(this, this);
 
         BukkitScheduler scheduler = getServer().getScheduler();
+
+        // Schedule a check after the first minute. Then repeated at the specified delay.
         scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-                // Do something
-                getLogger().info("Running player checker.");
                 checkPlayers();
             }
-        }, 0L, 120L);
+        }, 1200L, delay);
     }
     // Skim through all players and check for expired ones.
     public void checkPlayers() {
+        getLogger().info("Checking for expired players.");
         List<OfflinePlayer> players = new ArrayList<OfflinePlayer>();
+
+        // Populate the list with players that haven't been on for the specified threshold.
         for (OfflinePlayer p : getServer().getOfflinePlayers()) {
-            getLogger().info("Checking "+p.getUniqueId());
             long secondsSince = epoch() - (p.getLastPlayed()/1000);
             if (secondsSince >= threshold) {
-                getLogger().info(p.getUniqueId()+" hasn't been on for "+secondsSince+" seconds, Higher than "+threshold);
                 players.add(p);
-            } else {
-                getLogger().info(p.getUniqueId()+" was last on "+secondsSince+" ago!");
             }
         }
+
+        // If there's any players. Run the event.
         if (players.size() > 0) {
-            getLogger().info("Calling event for "+players.size());
             PlayerExpiredEvent event = new PlayerExpiredEvent(players);
             getServer().getPluginManager().callEvent(event);
             if (!event.isCancelled()) {
-                // Remove the player data
-                getLogger().info("Removing data for all players.");
+                // Remove the player data, Assuming the event isn't cancelled!
+                getLogger().info("Removing data for all "+players.size()+" players.");
+
+                // Get the main world folder.
+                File BaseFolder = new File(getServer().getWorlds().get(0).getWorldFolder(), "playerdata");
+                for (OfflinePlayer p : players) {
+                    // Get and remove the players file.
+                    File playerFile = new File(BaseFolder, p.getUniqueId()+".dat");
+                    if (playerFile.exists()) {
+                        playerFile.delete();
+                    }
+                }
             }
-        } else {
-            getLogger().info("No players to remove data for. Skipping event.");
         }
     }
 
+    @EventHandler
     public void onExpire(PlayerExpiredEvent event) {
         getLogger().info("Expire event called, Im a child plugin.");
     }
