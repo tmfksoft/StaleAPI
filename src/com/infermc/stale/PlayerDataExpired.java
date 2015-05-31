f9d620ecc852071fe6449040d3b857f37e5fec5e
Thomas Edwards <thomas.edwards@xeonr.com>
2015-05-31 23:05:16 +0100

    Added some methods etc

--- src/com/infermc/stale/PlayerDataExpired.java
+++ src/com/infermc/stale/PlayerDataExpired.java
@@ -1,12 +1,41 @@
 package com.infermc.stale;
 
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Thomas on 31/05/2015.
  */
-public final class PlayerDataExpired implements Listener {
-    public boolean isCheese() {
-        return true;
+public final class PlayerDataExpired extends Event {
+    private boolean canceled = false;
+    private List<UUID> players;
+    private static final HandlerList handlers = new HandlerList();
+
+    public PlayerDataExpired(List<UUID> plist) {
+        players = plist;
+    }
+
+    /* Getters */
+    public boolean isCanceled() {
+        return canceled;
+    }
+    public List<UUID> getPlayers(){
+        return players;
+    }
+
+    /* Setters */
+    public void setCanceled(boolean state) {
+        canceled = state;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
--- src/com/infermc/stale/StaleAPI.java
+++ src/com/infermc/stale/StaleAPI.java
@@ -3,6 +3,10 @@ package com.infermc.stale;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Thomas on 31/05/2015.
  */
@@ -10,5 +14,17 @@ public class StaleAPI extends JavaPlugin implements Listener {
     public void onEnable() {
         getLogger().info("StaleAPI Loaded. :D");
         getServer().getPluginManager().registerEvents(new PlayerDataExpired(), this);
+    }
+    // Skim through all players and check for expired ones.
+    public void checkPlayers() {
+        List<UUID> players = new ArrayList<UUID>();
+        PlayerDataExpired event = new PlayerDataExpired(players);
+        getServer().getPluginManager().callEvent(event);
+        if (!event.isCanceled()) {
+
+        }
+    }
+
+    public void onExpire(PlayerDataExpired event) {
     }
 }
