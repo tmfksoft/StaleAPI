645fbce6f88ed34c33195a7c8bad3045b125e556
Thomas Edwards <thomas.edwards@xeonr.com>
2015-06-02 12:45:42 +0100

    Added Vault Support Added PlayerExpiredEvent to signal when Player data actually expires. Added automatic exemption to Opped Players Added skipPlayer to PlayerExpireEvent to allow temporary exemption for specific players.
    (Still testing Vault Support)

---
+++ src/com/infermc/stale/PlayerExpiredEvent.java
@@ -0,0 +1,42 @@
+package com.infermc.stale;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.List;
+
+/**
+ * Called when a players data has already expired. Intend to tell plugins to remove their player data. (Cannot be cancelled)
+ * @author Thomas Edwards (MajesticFudgie)
+ */
+public final class PlayerExpiredEvent extends Event {
+
+    private List<OfflinePlayer> players;
+
+    // Bukkit stuff
+    private static final HandlerList handlers = new HandlerList();
+
+    public PlayerExpiredEvent(List<OfflinePlayer> plist) {
+        players = plist;
+    }
+
+    /* Getters */
+
+    /**
+     * Get a list of players who's data is about to expire.
+     * @return
+     * List of players.
+     */
+    public List<OfflinePlayer> getPlayers(){
+        return players;
+    }
+
+    // Essential stuff.
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
